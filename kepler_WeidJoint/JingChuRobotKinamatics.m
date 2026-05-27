b5a7772bbfc245b65f230210a08f03659f72a54f
erhdo16 <1870844769@QQ.com>
2026-05-27 20:15:25 +0800

    Create JingChuRobotKinamatics.m

---
+++ kepler_WeidJoint/JingChuRobotKinamatics.m
@@ -0,0 +1,12 @@
+
+%% bachmark
+robot = importrobot('JingChuJointR.urdf');
+
+% robot.show();
+robot.showdetails();
+
+% % tf = getTransform(robot, homeConfig, 'left_hip_roll')
+
+
+addpath('./modernRobot/mr')
+addpath('./kepler_new')
